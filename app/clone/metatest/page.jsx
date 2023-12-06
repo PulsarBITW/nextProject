f99423ec85cbe119593dfa26fbe2c49865ab4741
PulsarBITW <GordeyBITW@gmail.com>
2023-12-06 15:59:07 +0300

    8

---
+++ app/clone/metatest/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "metatest",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h2>metaTest</h2>
+      <p>{"metadata - работает, как и наследование у js"}</p>
+    </div>
+  );
+}
