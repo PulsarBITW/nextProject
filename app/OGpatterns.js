5455a79103f95d95f7c0042175e58756ff141da5
PulsarBITW <GordeyBITW@gmail.com>
2023-12-06 17:19:55 +0300

    29

---
+++ app/OGpatterns.js
@@ -0,0 +1,10 @@
+export const secondOG = {
+  description: "og description from OGPatterns",
+  images: [
+    {
+      url: "https://i.postimg.cc/DwfwGF7t/3.png",
+      width: 800,
+      height: 500,
+    },
+  ],
+};
--- app/clone/metatest/page.jsx
+++ app/clone/metatest/page.jsx
@@ -1,7 +1,10 @@
+import { secondOG } from "@/app/OGpatterns";
+// OG - наследуется, НО перезатирается ЦЕЛИКОМ, а не отдельными полями
 export const metadata = {
   title: "metatest",
   openGraph: {
-    title: "og metatest",
+    title: "OG metatest",
+    ...secondOG,
   },
 };
 
@@ -10,6 +13,11 @@ export default function Page() {
     <div>
       <h2>metaTest</h2>
       <p>{"metadata - работает, как и наследование у js"}</p>
+      <p>
+        {
+          "НО OpenGraph - наследуется, НО перезатирается целиком, то есть если мы хотим поменять например title в OG - то прийдется вводить ВСЕ поля openGraph - так как OG - ПЕРЕЗАТИРАЕТСЯ ЦЕЛИКОМ"
+        }
+      </p>
     </div>
   );
 }
